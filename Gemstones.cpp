fb7a1d92e7428db8d535833bb177362d3651af8d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-21 01:01:29 +0900

    Update Gemstones.cpp

--- Gemstones.cpp
+++ Gemstones.cpp
@@ -1,1 +1,33 @@
+int gemstones(vector<string> arr)
+{
+    int gems_count = 0;
+
+    std::map<int, int> mp;
+    
+
+    for (const auto& rock : arr)
+    {
+        std::set<int> st;
+
+        for (size_t i = 0; i < rock.size(); ++i)
+        {
+            st.insert(rock[i] - 'a');
+        }
+
+        for (auto itr = st.cbegin(); itr != st.cend(); ++itr)
+        {
+            mp[*itr]++;
+        }
+    }
+
+    for (auto itr = mp.cbegin(); itr != mp.cend(); ++itr)
+    {
+        if (itr->second == arr.size())
+        {
+            gems_count++;
+        }
+    }
+
+    return gems_count;
+}
 
