990da2031c9f8505c555c832ee31095168606b3a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-04 04:50:59 +0900

    Update Gemstones.cpp

--- Gemstones.cpp
+++ Gemstones.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gemstones' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING_ARRAY arr as parameter.
+ */
+
 int gemstones(vector<string> arr)
 {
     int gems_count = 0;
