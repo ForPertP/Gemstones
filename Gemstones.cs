10ffb21d341cb273e6930eb8939c80d5bfe1bc28
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-12 07:02:43 +0900

    Update Gemstones.cs

--- Gemstones.cs
+++ Gemstones.cs
@@ -1,1 +1,29 @@
-Gemstones
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gemstones' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY arr as parameter.
+     */
+
+    public static int gemstones(List<string> arr)
+    {
+    }
+
+}
