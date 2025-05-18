58178ba389495b247d8b8d4a65f2caa9db489ecf
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-19 06:15:38 +0900

    Update Gemstones.java

--- Gemstones.java
+++ Gemstones.java
@@ -33,5 +33,15 @@ class Result {
 
         return common.cardinality();
     }
+
+
+    public static int popCount(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
     
 }
