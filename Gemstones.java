de9cb5047506a399f4fbdbb9683e4b750c2096b9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-22 04:26:16 +0900

    Update Gemstones.java

--- Gemstones.java
+++ Gemstones.java
@@ -84,3 +84,29 @@ class Result {
     }
 }
 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int result = Result.gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
